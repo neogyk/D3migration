bf387f7667cb0f34d11ee8db944b64a5097e37f0
Leonid Didukh <ledidukH@gmail.com>
2017-09-29 15:08:48 +0300

    Added First Layer of migrated person's

--- ui/dataviz/my-app/src/app/app.component.ts
+++ ui/dataviz/my-app/src/app/app.component.ts
@@ -4,6 +4,9 @@ import 'rxjs/add/operator/toPromise';
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
 import * as d3Geo from 'd3-geo';
+import * as d3Scale from 'd3-scale';
+import * as d3Array from 'd3-array';
+import * as d3Axes from  'd3-axis';
 
 @Component({
   selector: 'app-root',
@@ -11,81 +14,57 @@ import * as d3Geo from 'd3-geo';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Map of Migration in January';
 
   constructor (private http:Http){
 
     var width = 960;
     var height = 1160;
 
+
+    var projection = d3Geo.geoAlbers()
+                     .scale(3000).translate([width /2, height/2 ])
+                     .center([30, 45])
+                     .rotate([-1, 2])
+                     .parallels([50, 60]);
+
+     var path = d3Geo.geoPath().projection(projection);
+
+
     var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-    d3.json("./assets/ukraine.json", function(error, uk) {
-
-                    console.log(uk);
-                }
-            );
 
     d3.json("./assets/ukraine.json", function(error, uk) {
       svg.append("path")
       .datum(topojson.feature(uk, uk['objects']['ukrunits1']))
-      .attr("d", d3Geo.geoPath().projection(
-        d3Geo.geoAlbers().scale(3000).translate([width /2, height/2 ])
-             .center([30, 45])
-             .rotate([-1, 2])
-             .parallels([50, 60])
-      ));
-      //.attr("class", function(d) { return "ukrunits1 " + d.id; })
+      .attr("d", path);
 
       svg.selectAll(".subunit")
            .data(topojson.feature(uk, uk['objects']['ukrunits1']).features)
            .enter().append("path")
-            .attr('fill','blue')
-            .dattr("d",  d3Geo.geoPath().projection(
-            d3Geo.geoAlbers().scale(3000)
-             .translate([width /2, height/2 ])
-             .center([30, 45])
-             .rotate([-1, 2])
-             .parallels([50, 60])
-      ));
-      var path = d3Geo.geoPath().projection(
-        d3Geo.geoAlbers().scale(6000)
-             .translate([width /2, height/2 ])
-             .center([0, 55.4])
-             .rotate([4.4, 0])
-             .parallels([50, 60]));
+           .attr('stroke',"black")
+           .attr("d",  path);
+      d3.json('./assets/migration.json', function (error, migration) {
+        var color = d3Scale.scaleLinear<string>()
+                           .domain([0, new Number(d3Array.max(migration['all']))])
+                           .range(['#F08080',"#A0522D"]);
 
-      var projection = d3.geoAlbers()
-            .center([0, 55.4])
-            .rotate([4.4, 0])
-            .parallels([50, 60])
-            .scale(6000)
-            .translate([width / 2, height / 2]);
-      /*
-      svg.append("path")
-      .datum(topojson.mesh(uk, uk['objects']['ukrunits1'], function(a, b) { return a !== b && a.id !== "IRL"; }))
-      .attr("d", path)
-      .attr("class", "subunit-boundary");
+        /*
+        var _color = d3Scale.scaleLinear()
+                           .domain([0,
+                                    d3Array.max(migration['all'])])
+                           .range([0,
+                                   d3Array.max(migration['all'])]);
+         */
 
-      svg.append("path")
-        .datum(topojson.feature(uk, uk['objects']['places']))
-        .attr("d", path)
-        .attr("class", "place");
+        console.log('Migration', migration);
+        svg.selectAll('path').data(migration['all'])
+          .attr('fill', function(d){return color(new Number(d)); });
 
-      svg.selectAll(".place-label")
-        .data(topojson.feature(uk, uk['objects']['places']).features)
-        .enter().append("text")
-        .attr("class", "place-label")
-        .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-        .attr("dy", ".35em")
-        .text(function(d) { return d.properties.name; });
-      */
-
-    //svg.selectAll(".place-label")
-    //.attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
-    //.style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; });
+        //svg.append("g").call(d3Axes.axisLeft(_color));
+      })
 
     });
 
