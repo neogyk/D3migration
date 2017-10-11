51f0daf9724b57b890654025fa75581cd2e6bc4e
Leonid Didukh <ledidukH@gmail.com>
2017-10-11 18:14:02 +0300

    Fixed Brush and added region-map interactive effects

--- ui/dataviz/my-app/src/app/app.component.ts
+++ ui/dataviz/my-app/src/app/app.component.ts
@@ -21,7 +21,8 @@ export class AppComponent {
 
     var width = 960;
     var height = 1160;
-
+    var migration:any;
+    var color:any;
 
     var projection = d3Geo.geoAlbers()
                      .scale(3000).translate([width /2, height/2 ])
@@ -42,10 +43,8 @@ export class AppComponent {
                   .attr('width',1500).attr('height', 120);
 
     var brush = d3.brushX()
-                  .extent([[0, 0], [200, 200]])
-                  .on("start brush end", function () {
-
-                  });
+                  .extent([[0, 0], [1500, 200]])
+                  .on("start brush end", brushMoved);
 
     var gBrush = timeline.append("g")
                 .attr("class", "brush")
@@ -63,40 +62,75 @@ export class AppComponent {
               .data(topojson.feature(uk, uk['objects']['ukrunits1']).features)
               .enter()
               .append("path")
-              .attr("d",  path);
+              .attr('class','region')
+              .attr("d",  path)
+              .on('mouseover', handleRegionSel)
+              .on('mouseout', outhandleRegionSel);
+        //Modification of map circle
+          svg.selectAll(".subunit").append('g')
+            .data(topojson.feature(uk, uk['objects']['ukrunits1']).features)
+            .enter()
+            .append('circle')
+            .attr("r", 3.5);
 
+        //add interactive widget for selecting region
+       //
+      //
+    //
+      //
+      d3.json('./assets/migration.json', function (error, migration) {
 
-          d3.json('./assets/migration.json', function (error, migration) {
-
+            this.migration = migration;
             var length = migration['all'].length;
             var max = new Number(d3Array.max(migration['all']));
-            var color = d3Scale.scaleLinear<string>()
+            this.color = d3Scale.scaleLinear<string>()
                            .domain([0, max])
                            .range(["#3b5682", '#a01e1e']);
 
             var x = d3Scale.scaleLinear()
-                           .domain([0, length])
+                           .domain([0, max])
                            .range([0, 1500]);
             var y = d3Scale.scaleLinear()
                            .domain([0, max])
-                           .range([0,  120]);
+                           .range([0,  200]);
 
 
              var line = d3.line()
                           .x(function(d,i) { return x(i); })
                           .y(function(d, i) {return y(new Number(d)); });
 
-            gBrush.call(brush.move, [0.3, 0.5].map(x));
+              svg.selectAll('path').append('g').selectAll("circle")
+                            .data(migration['all'])
+                            .enter().append("circle").attr("r", 3.5);;
 
-            timeline.append('path')
-                    .attr("class", "line").attr('d', line(migration['all']))
-                    .attr('stroke', 'blue');
+              var circle = timeline.append("g")
+                            .attr("class", "circle")
+                            .selectAll("circle")
+                            .data(migration['all'])
+                            .enter().append("circle")
+                            .attr("transform", function(d) { return "translate(" + x(new Number(d)) + "," + y(new Number(d)) + ")"; })
+                            .attr("r", 3.5);
+
+
+            //timeline.append('path')
+            //        .attr("class", "line").attr('d', line(migration['all']))
+            //       .attr('stroke', 'blue');
 
             svg.selectAll('path').data(migration['all'])
-                .attr('stroke', function(d){return color(new Number(d)); });
+                .attr('fill', function(d){return this.color(new Number(d)); })
+                .attr('stroke','black');
 
+
+            gBrush.call(brush.move, [0.3, 0.5].map(x));
         });
     });
+
+  function brushMoved(){};
+  function handleRegionSel(){
+      d3.select(this).attr('fill','orange');};
+  function outhandleRegionSel(d,i){
+      var new_d = this.migration['all'][i];
+      d3.select(this).attr('fill', function(d){return color(new Number(new_d)); })};
 
 
   }
